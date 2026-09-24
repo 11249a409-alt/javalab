b7fe2d68ed99f103e7fc45a1b74a09b7fdb3860f
11249a409-alt <11249a409@kanchiuniv.ac.in>
2026-09-24 11:40:32 +0530

    Add UseMath.java to demonstrate MathUtil methods

---
+++ Program 2.2 — UseMath.java
@@ -0,0 +1,12 @@
+// File: UseMath.java
+import com.scsvmv.util.MathUtil;
+
+public class UseMath {
+    public static void main(String[] args) {
+        System.out.println("Square of 7 = " + MathUtil.square(7));
+        System.out.println("Cube of 3 = " + MathUtil.cube(3));
+    }
+}
+Output:
+Square of 7 = 49
+Cube of 3 = 27
